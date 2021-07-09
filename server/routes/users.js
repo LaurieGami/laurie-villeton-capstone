8437e778ca379ee3dc3d1b5566d08949cca53f66
LaurieGami <laurievilleton@gmail.com>
2021-07-08 21:38:30 -0700

    started setting up Auth

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,31 +1,75 @@
 const express = require('express');
-const User = require('../models/user');
 const router = express.Router();
+const User = require('../models/user');
 
-router.route('/users')
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+// const authorize = require('../middlewares/auth');
+require('dotenv').config();
+
+const SALT_ROUNDS = 8;
+const JWT_SECRET = process.env.JWT_SECRET;
+
+const signJWTToken = user => {
+    const token = jwt.sign(
+        {
+            id: user.id,
+            sub: user.attributes.email
+        },
+        JWT_SECRET,
+        { expiresIn: '8h' }
+    );
+
+    return token;
+};
+
+router.route('/register') // POST - /users
     .post((req, res) => {
-        new User({
-            firstName: req.body.firstName,
-            lastName: req.body.lastName,
-            phone: req.body.phone,
-            email: req.body.email,
-            address: req.body.address,
-            city: req.body.city,
-            province: req.body.province,
-            country: req.body.country
-        })
-            .save()
-            .then((newUser) => {
-                res.status(201).json(newUser);
+        const { password } = req.body;
+
+        bcrypt.hash(password, SALT_ROUNDS, (err, hashedPassword) => {
+            if (err) return res.status(500).json({ message: "Couldn't encrypt the password" });
+
+            new User({
+                ...req.body,
+                password: hashedPassword
             })
-            .catch((err) =>
-                res.status(400).json({ message: "Error, can't create a new user", error: err })
-            );
+                .save()
+                .then((newUser) => {
+                    const token = signJWTToken(newUser);
+
+                    return res.status(201).json({ authToken: token });
+                })
+                .catch((err) => {
+                    return res.status(400).json({ message: "Couldn't create a new user. Please check your inputs.", error: err });
+                });
+        });
     });
 
-router.route('/users/:userId')
+router.route('/login')
+    .post((req, res) => {
+        const { email, password } = req.body;
+
+        User.where({ email })
+            .fetch()
+            .then(user => {
+                bcrypt.compare(password, user.attributes.password, (_, success) => {
+                    if (success) {
+                        const token = signJWTToken(user);
+                        return res.status(200).json({ authToken: token });
+                    } else {
+                        return res.status(403).json({ message: 'Username/password combination is wrong' });
+                    }
+                })
+            })
+            .catch(() => {
+                return res.status(400).json({ message: "User is not found" });
+            })
+    })
+
+router.route('/users/:userId') // GET - /profile
     .get((req, res) => {
-        User.where({ id: req.params.userId }) // add { withRelated: ['trips'] } if you want trips too
+        User.where({ id: req.params.userId })
             .fetch()
             .then((user) => {
                 res.status(200).json(user);
@@ -39,22 +83,22 @@ router.route('/users/:userId')
             .fetch()
             .then((user) => {
                 user
-                .save({
-                    firstName: req.body.firstName,
-                    lastName: req.body.lastName,
-                    phone: req.body.phone,
-                    email: req.body.email,
-                    address: req.body.address,
-                    city: req.body.city,
-                    province: req.body.province,
-                    country: req.body.country
-                })
-                .then((updatedUser) => {
-                    res.status(200).json(updatedUser);
-                })
-                .catch((err) => {
-                    res.status(400).json({ message: `Error, can't update user with userId of ${req.params.userId}`, error: err })
-                });
+                    .save({
+                        firstName: req.body.firstName,
+                        lastName: req.body.lastName,
+                        phone: req.body.phone,
+                        email: req.body.email,
+                        address: req.body.address,
+                        city: req.body.city,
+                        province: req.body.province,
+                        country: req.body.country
+                    })
+                    .then((updatedUser) => {
+                        res.status(200).json(updatedUser);
+                    })
+                    .catch((err) => {
+                        res.status(400).json({ message: `Error, can't update user with userId of ${req.params.userId}`, error: err })
+                    });
             });
     });
 
